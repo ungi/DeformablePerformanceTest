094f08b38376ef4e552c564919828f2ca0389155
Tamas Ungi <ungi.tamas@gmail.com>
2018-07-20 10:22:26 -0400

    The test can probably not get much simpler.

--- SimpleTpsPerformance.py
+++ SimpleTpsPerformance.py
@@ -6,15 +6,36 @@ import math
 import numpy
 from os.path import expanduser, join
 
+# Parameters to control test size
 
 Scale = 100.0
 WaveScale = 5.0
-NumPerEdge = 5         # Number of TPS point pairs will be the cube of this number
+NumPerEdge = 6         # Number of TPS point pairs will be the cube of this number
 ModelResolution = 200  # Higher number will create a more detailed model
+
+# Start profiler
 
 Profiler = cProfile.Profile()
 Profiler.enable()
 
+# Create a model to be transformed
+
+SphereSource = vtk.vtkSphereSource()
+SphereSource.SetRadius(Scale / 2.0)
+SphereSource.SetThetaResolution(ModelResolution)
+SphereSource.SetPhiResolution(ModelResolution)
+SphereSource.Update()
+
+ModelNode = slicer.vtkMRMLModelNode()
+slicer.mrmlScene.AddNode( ModelNode )
+ModelNode.SetName( "ModelNode" )
+ModelNode.SetAndObservePolyData( SphereSource.GetOutput() )
+
+DisplayNode = slicer.vtkMRMLModelDisplayNode()
+slicer.mrmlScene.AddNode(DisplayNode)
+ModelNode.SetAndObserveDisplayNodeID(DisplayNode.GetID())
+
+# Create deformable transform
 
 FromPoints = vtk.vtkPoints()
 ToPoints = vtk.vtkPoints()
@@ -34,62 +55,35 @@ for x in range(NumPerEdge):
       cz = pos_z * Scale
       ToPoints.InsertNextPoint(cx, cy, cz)
 
-
 tNode = slicer.vtkMRMLTransformNode()
 tNode.SetName( 'TpsTransform' )
 slicer.mrmlScene.AddNode( tNode )
-
-tNode2 = slicer.vtkMRMLTransformNode()
-tNode2.SetName( 'TpsTransform2' )
-slicer.mrmlScene.AddNode( tNode2 )
-
-tNode2.SetAndObserveTransformNodeID(tNode.GetID())
-
-# Create a model to be transformed
-
-SphereSource = vtk.vtkSphereSource()
-SphereSource.SetRadius(Scale / 2.0)
-SphereSource.SetThetaResolution(ModelResolution)
-SphereSource.SetPhiResolution(ModelResolution)
-SphereSource.Update()
-
-ModelNode = slicer.vtkMRMLModelNode()
-slicer.mrmlScene.AddNode( ModelNode )
-ModelNode.SetName( "ModelNode" )
-ModelNode.SetAndObservePolyData( SphereSource.GetOutput() )
-
-DisplayNode = slicer.vtkMRMLModelDisplayNode()
-slicer.mrmlScene.AddNode(DisplayNode)
-ModelNode.SetAndObserveDisplayNodeID(DisplayNode.GetID())
-
-ModelNode.SetAndObserveTransformNodeID(tNode2.GetID())
-
 
 tps = vtk.vtkThinPlateSplineTransform()
 tps.SetSourceLandmarks( FromPoints )
 tps.SetTargetLandmarks( ToPoints )
 tps.SetBasisToR()
 
-tps2 = vtk.vtkThinPlateSplineTransform()
-tps2.SetSourceLandmarks( FromPoints )
-tps2.SetTargetLandmarks( ToPoints )
-tps.SetBasisToR()
+# Apply deformable transform on model
 
 tNode.SetAndObserveTransformToParent( tps )
-tNode2.SetAndObserveTransformToParent( tps2 )
+ModelNode.SetAndObserveTransformNodeID(tNode.GetID())
 
+# Print profiler output
 
 Profiler.disable()
 StatsString = StringIO()
-SortBy = u'cumtime'
+SortBy = u'name'
 ProfileStats = pstats.Stats(Profiler, stream=StatsString).sort_stats(SortBy)
 ProfileStats.print_stats()
 out = StatsString.getvalue()
 print(out)
 
 home = expanduser("~")
-FileName = join( home, "TpsTest_{0}.txt".format(str(datetime.datetime.now())).replace(':', '_') )
+FileName = "TpsTest_{0}_Slicer-{1}.{2}.txt".format(
+  str(datetime.datetime.now()), slicer.app.majorVersion, slicer.app.minorVersion).replace(':', '_')
+FilePathName = join(home, FileName)
 
-
-with open(FileName, 'w') as outfile:
+with open(FilePathName, 'w') as outfile:
   outfile.write(out)
+
